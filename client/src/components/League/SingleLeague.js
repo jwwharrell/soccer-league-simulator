41ee6593717a1fd90a396ed55002b743e412d0b0
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-18 17:17:55 -0500

    Made a club dropdown for adding clubs to leagues

--- client/src/components/League/SingleLeague.js
+++ client/src/components/League/SingleLeague.js
@@ -5,7 +5,12 @@ import { Link } from 'react-router-dom'
 export default class SingleLeague extends Component {
     state = {
         league: '',
-        allClubs: []
+        currentClubs: [],
+        allClubs: [],
+        filteredClubs: [],
+        selectedClub: '',
+        filterButtonClicked: true,
+        seasonLength: 4
     }
 
 
@@ -16,18 +21,81 @@ export default class SingleLeague extends Component {
     getLeague = async () => {
         let leagueId = this.props.match.params.leagueId
         let res = await axios.get(`/api/league/${leagueId}`)
-        this.setState({ league: res.data.singleLeague, allClubs: res.data.allClubs })
+        let secondRes = await axios.get('/api/club/')
+        this.setState({ league: res.data.singleLeague, currentClubs: res.data.allClubs, allClubs: secondRes.data, filteredClubs: secondRes.data })
     }
 
+    onSelectChange = (e) => {
+        let clubId = e.target.value
+        let selectedClub = this.state.allClubs.filter((club) => {
+            return club._id === clubId
+        })[0]
+        this.setState({selectedClub})
+    }
 
+    onSelectSubmit = (e) => {
+        e.preventDefault()
+        let club = this.state.selectedClub
+        club.leagueId = this.state.league._id
+        axios.put(`/api/club/${club._id}`, club)
+        this.getLeague()
+    }
+
+    onFilterClubClick = () => {
+        const previousState = { ...this.state }
+        previousState.filterButtonClicked = !this.state.filterButtonClicked
+        this.setState(previousState)
+        if (this.state.filterButtonClicked) {
+            this.filterClubs()
+        } else {
+            this.showAllClubs()
+        }
+    }
+
+    filterClubs = () => {
+        let filteredClubs = this.state.allClubs.filter((club) => {
+            return club.leagueId !== this.state.league._id
+        })
+        this.setState({ filteredClubs })
+    }
+
+    showAllClubs = () => {
+        let filteredClubs = this.state.allClubs
+        this.setState({ filteredClubs })
+    }
 
     render() {
         return (
             <div>
                 <h1>{this.state.league.name}</h1>
+                <form
+                    onSubmit={this.onSelectSubmit}
+                >
+                    <select
+                        onChange={this.onSelectChange}
+                    >
+                        <option
+                            value={null}
+                        >{this.state.filteredClubs.length ? '---Select A Club---' : '---No Clubs Available---'}</option>
+                        {this.state.filteredClubs.map((club) => {
+                            return (
+                                <option value={club._id} key={'dropdown-key-' + club._id}>{club.name}</option>
+                            )
+                        })}
+                    </select>
+                    <input
+                        type='submit'
+                        disabled={!this.state.selectedClub}
+                    />
+                </form>
+                <button
+                    onClick={this.onFilterClubClick}
+                >
+                    {this.state.filterButtonClicked ? `Hide ${this.state.league.name} Clubs` : 'Show All Clubs'}
+                </button>
                 <h2>Clubs</h2>
                 <ol>
-                    {this.state.allClubs.map((club) => {
+                    {this.state.currentClubs.map((club) => {
                         let clubLink = `/club/${club._id}`
                         return (
                             <Link to={clubLink} key={club._id}><li>{club.name}</li></Link>
