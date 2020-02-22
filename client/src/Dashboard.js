58ba966edc8e90b15ed9bc51b23ef049a09cdcd9
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-22 17:18:54 -0500

    Created a table for league results

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Dashboard extends Component {
+
     render() {
         return (
             <div>
+                <h1>League Simulator</h1>
                 <Link to='/league'><h2>All Leagues</h2></Link>
                 <Link to='/club'><h2>All Clubs</h2></Link>
                 <Link to='/player'><h2>All Players</h2></Link>
--- client/src/components/League/Schedule/Schedule.js
+++ client/src/components/League/Schedule/Schedule.js
@@ -1,36 +1,89 @@
 import React, { Component } from 'react'
+import DataTable from 'react-data-table-component'
 
 
 
 export default class Schedule extends Component {
     state = {
-        schedule: []
+        schedule: [],
+        newSeasonClicked: false,
+        fixtureResults: [],
+        leagueTable: []
     }
 
     createSchedule = () => {
         let allClubs = this.props.allClubs
         let schedule = []
         let week = 1
+        let leagueTable = allClubs
         allClubs.map((club) => {
             allClubs.forEach((clubTwo) => {
                 if (club._id !== clubTwo._id) {
-                    schedule.push(`Week ${week} ${clubTwo.name} @ ${club.name}`)
+                    schedule.push({ home: club, away: clubTwo, week: week })
                     week++
                 }
             })
         })
-        this.setState({ schedule })
+        leagueTable.map((club) => {
+            club.points = 0
+            club.wins = 0
+            club.draws = 0
+            club.losses = 0
+        })
+        this.setState({ schedule, leagueTable })
+
+    }
+
+    handleNewSeason = () => {
+        const newSeasonClicked = !this.state.newSeasonClicked
+        this.setState({ newSeasonClicked })
     }
 
     render() {
         let schedule = this.state.schedule
+        const data = this.state.leagueTable
+        const columns = [
+            {
+                name: 'Club',
+                selector: 'name',
+                sortable: true
+            },
+            {
+                name: 'Points',
+                selector: 'points',
+                sortable: true
+            },
+            {
+                name: 'Wins',
+                selector: 'wins',
+                sortable: true
+            },
+            {
+                name: 'Draws',
+                selector: 'draws',
+                sortable: true
+            },
+            {
+                name: 'Losses',
+                selector: 'losses',
+                sortable: true
+            },
+        ]
         return (
             <div>
                 <h2>Schedule</h2>
-                <button
-                    onClick={this.createSchedule}
-                >Create Schedule</button>
-                {schedule.length ?
+                {!schedule.length ?
+                    <button
+                        onClick={this.createSchedule}
+                    >
+                        Create Schedule
+                </button>
+                    : <button
+                        onClick={this.handleNewSeason}
+                    >Simulate Season</button>
+                }
+
+                {/* {schedule.length ?
                     <div className='schedule'>
                         {schedule.map((week) => {
                             return(
@@ -39,7 +92,17 @@ export default class Schedule extends Component {
                         })}
                     </div>
                 : null
+                } */}
+                {schedule.length ?
+                    <DataTable
+                        className='table'
+                        title="League Table"
+                        columns={columns}
+                        data={data}
+                    />
+                    : null
                 }
+
             </div>
         )
     }
