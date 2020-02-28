16884681dfe4b9949680456230ab14e94784adfe
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-28 14:34:58 -0500

    Building test data outside of backend

---
+++ client/test_data.js
@@ -0,0 +1,50 @@
+export default {
+    continents: [
+        {
+            name: 'Europe',
+            countries: [
+                {
+                    name: 'England',
+                    leagues: [
+                        {
+                            name: 'Premier League',
+                            pyramidPos: 1,
+                            clubs: [
+                                {
+                                    name: 'Tottenham Hotspur',
+                                    players: [
+                                        {
+                                            name: 'Harry Kane',
+                                            position: 'Striker',
+                                            skill: 89
+                                        }
+                                    ]
+                                },
+                                {
+                                    name: 'Arsenal',
+                                    players: [
+                                        {
+                                            name: 'Pierre-Emerick Aubameyang',
+                                            position: 'Striker',
+                                            skill: 88
+                                        }
+                                    ]
+                                },
+                                {
+                                    name: 'Chelsea',
+                                    players: [
+                                        {
+                                            name: `N'Golo Kante`,
+                                            position: 'Central Midfielder',
+                                            skill: 89
+                                        }
+                                    ]
+                                },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+    ]
+}
