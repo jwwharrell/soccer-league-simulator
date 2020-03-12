00aefd99e68de93c124f11a0765c2159f7184f33
Jordan Harrell <jwharrell0516@gmail.com>
2020-03-11 20:29:37 -0400

    cleaned up redundancies in LeagueTable component

--- client/src/components/SimulationArena/SimViews/LeagueTable.js
+++ client/src/components/SimulationArena/SimViews/LeagueTable.js
@@ -2,38 +2,7 @@ import React, { Component } from 'react'
 import DataTable from 'react-data-table-component'
 
 export default class LeagueTable extends Component {
-    getRandomInt = (max) => {
-        return Math.floor(Math.random() * Math.floor(max))
-    }
-
-    handleNewSeason = () => {
-        const previousState = { ...this.state }
-        previousState.schedule.map((week) => {
-            week.score.away = this.getRandomInt(5)
-            week.score.home = this.getRandomInt(6)
-            week.home.goalsFor += week.score.home
-            week.home.goalsAgainst += week.score.away
-            week.away.goalsFor += week.score.away
-            week.away.goalsAgainst += week.score.home
-            if (week.score.home === week.score.away) {
-                week.home.draws++
-                week.home.points++
-                week.away.draws++
-                week.away.points++
-            } else if (week.score.home > week.score.away) {
-                week.home.wins++
-                week.home.points += 3
-                week.away.losses++
-            } else {
-                week.away.wins++
-                week.away.points += 3
-                week.home.losses++
-            }
-        })
-
-        this.setState(previousState)
-    }
-
+    
     render() {
         const data = this.props.league.clubs
         const columns = [
